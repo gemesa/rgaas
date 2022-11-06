07e40fd063c4ea2f4cebd11edb1a097133c5349c
gemesa <59890178+gemesa@users.noreply.github.com>
2022-11-06 22:07:30 +0100

    feat: remove time and hostname data from logger

--- modules/logger/logger.c
+++ modules/logger/logger.c
@@ -2,43 +2,17 @@
 // Created by gemesa on 11/3/22.
 //
 
-#include <netdb.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
-#include <time.h>
-#include <unistd.h>
 
 #include "logger.h"
-
-enum
-{
-    HOSTNAME_SIZE = 50
-};
 
 static unsigned int calc_pri(int facility, int severity)
 {
     return facility + severity;
-}
-
-static void get_time(char *time_formatted)
-{
-    time_t time_raw;
-    (void) time(&time_raw);
-    char time_tmp[sizeof "2022-11-02T08:00:00+01:00"];
-    (void) strftime(time_tmp, sizeof time_tmp, "%FT%T+01:00", gmtime(&time_raw));
-    (void) strncpy(time_formatted, time_tmp, sizeof(time_tmp));
-}
-
-static void get_h_name(char *h_name)
-{
-    char hostname[HOSTNAME_SIZE] = {[0 ... HOSTNAME_SIZE - 1] = '\0'};
-    gethostname(hostname, HOSTNAME_SIZE - 1);
-    struct hostent *h;
-    h = gethostbyname(hostname);
-    (void) strncpy(h_name, h->h_name, HOSTNAME_SIZE);
 }
 
 static int logger_open(void *s, char *filename, char *modes)
@@ -82,11 +56,7 @@ static int logger_write(void *s, char *msg, int facility, int severity)
     {
         syslog(LOG_NOTICE, "%s", msg);
     }
-    char time[sizeof "2022-11-02T08:00:00+01:00"];
-    char h_name[HOSTNAME_SIZE];
-    get_time(time);
-    get_h_name(h_name);
-    int status = fprintf(self->file, "%d %s %s rgaas - - - %s\n", calc_pri(facility, severity), time, h_name, msg);
+    int status = fprintf(self->file, "%d rgaas - %s\n", calc_pri(facility, severity), msg);
     return status;
 }
 

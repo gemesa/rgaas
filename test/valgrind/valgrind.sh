bedf2252aa725e119c2fe76712667dbdbd6a8caf
gemesa <59890178+gemesa@users.noreply.github.com>
2022-11-07 11:24:20 +0100

    ci: update valgrind test according to signal_handler

---
+++ test/valgrind/valgrind.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "valgrind starting..."
+valgrind --leak-check=full \
+            --show-leak-kinds=all \
+            --track-origins=yes \
+            --verbose \
+            --error-exitcode=1 \
+            --log-file="$(dirname "$0")/../../build/valgrind.log" \
+            "$(dirname "$0")/../../build/rgaas" > "$(dirname "$0")/../../build/rgaas.log" &
+
+valgrind_pid=$!
+echo "valgrind_pid: $valgrind_pid"
+
+echo "waiting for valgrind results... (sleep 15)"
+sleep 15
+
+kill -INT $valgrind_pid
+echo "SIGINT sent to valgrind_pid ($valgrind_pid)"
+
+wait $valgrind_pid

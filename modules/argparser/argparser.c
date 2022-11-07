16f49cfe16b6492d8f0b12d9612adc71916af1a2
gemesa <59890178+gemesa@users.noreply.github.com>
2022-11-07 13:05:42 +0100

    feat: list supplied non-option arguments

--- modules/argparser/argparser.c
+++ modules/argparser/argparser.c
@@ -11,12 +11,12 @@
 args_t argparse(int argc, char **argv)
 {
     args_t args = {.syslog_enabled = false, .log_file = NULL};
-    int c;
     char *usage_info = "Usage: %s [-s] [-l log_file]\n"
                        "  s: enable syslog (default: syslog is disabled)\n"
                        "  l: log file (default: process is logging to stdout, IMPORTANT: if -l is provided the destination directory has to exist)\n"
                        "Note: -s and/or -l should be provided together with -d as stdout and stderr are redirected to /dev/null in daemon mode\n";
 
+    int c;
     while ((c = getopt(argc, argv, "sl:h")) != -1)
     {
         switch (c)
@@ -34,6 +34,11 @@ args_t argparse(int argc, char **argv)
                 (void) fprintf(stderr, usage_info, argv[0]);
                 exit(EXIT_FAILURE);
         }
+    }
+
+    for (int index = optind; index < argc; index++)
+    {
+        printf("Non-option argument %s\n", argv[index]);
     }
 
     return args;

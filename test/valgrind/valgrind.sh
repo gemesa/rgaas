b69b57985e947d8863c849742966d7f58e47f684
gemesa <59890178+gemesa@users.noreply.github.com>
2022-11-07 23:35:22 +0100

    ci: add rgaas-client to valgrind.sh

--- test/valgrind/valgrind.sh
+++ test/valgrind/valgrind.sh
@@ -2,23 +2,37 @@
 
 echo "valgrind-server starting..."
 valgrind --leak-check=full \
-            --show-leak-kinds=all \
-            --track-origins=yes \
-            --verbose \
-            --error-exitcode=1 \
-            --log-file="$(dirname "$0")/../../build/valgrind-server.log" \
-            "$(dirname "$0")/../../build/rgaas-server" > "$(dirname "$0")/../../build/rgaas-server.log" &
+  --show-leak-kinds=all \
+  --track-origins=yes \
+  --verbose \
+  --error-exitcode=1 \
+  --log-file="$(dirname "$0")/../../build/valgrind-server.log" \
+  "$(dirname "$0")/../../build/rgaas-server" >"$(dirname "$0")/../../build/rgaas-server.log" &
 
-valgrind_pid=$!
-echo "valgrind_pid: $valgrind_pid"
+valgrind_server_pid=$!
 
-echo "waiting for valgrind results... (sleep 15)"
+echo "valgrind_server_pid: $valgrind_server_pid"
+
+echo "valgrind-client starting..."
+valgrind --leak-check=full \
+  --show-leak-kinds=all \
+  --track-origins=yes \
+  --verbose \
+  --error-exitcode=1 \
+  --log-file="$(dirname "$0")/../../build/valgrind-client.log" \
+  "$(dirname "$0")/../../build/rgaas-client" >"$(dirname "$0")/../../build/rgaas-client.log" &
+
+valgrind_client_pid=$!
+
+echo "valgrind_client_pid: $valgrind_client_pid"
+
+echo "waiting for valgrind-server and valgrind-client results... (sleep 15)"
 sleep 15
 
-kill -INT $valgrind_pid
-echo "SIGINT sent to valgrind_pid ($valgrind_pid)"
+kill -INT $valgrind_server_pid
+echo "SIGINT sent to valgrind_server_pid ($valgrind_server_pid)"
 
-echo "waiting for valgrind to finish writing the log file..."
+echo "waiting for valgrind-server and valgrind-client to finish writing the log file... (sleep 3)"
 sleep 3
 
 echo "valgrind-server.log:"
@@ -27,4 +41,27 @@ cat "$(dirname "$0")/../../build/valgrind-server.log"
 echo "rgaas-server.log:"
 cat "$(dirname "$0")/../../build/rgaas-server.log"
 
-wait $valgrind_pid
+echo "valgrind-client.log:"
+cat "$(dirname "$0")/../../build/valgrind-client.log"
+
+echo "rgaas-client.log:"
+cat "$(dirname "$0")/../../build/rgaas-client.log"
+
+wait $valgrind_server_pid
+server_code=$?
+
+wait $valgrind_client_pid
+client_code=$?
+
+echo ""
+echo "summary:"
+if [ "$server_code" -ne 0 ]; then
+  echo "valgrind-server failed"
+  exit 1
+elif [ "$client_code" -ne 0 ]; then
+  echo "valgrind-client failed"
+  exit 1
+else
+  echo "valgrind-server and valgrind-client passed"
+  exit 0
+fi
